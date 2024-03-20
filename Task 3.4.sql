25c69194717206f06397225551180b69175c0b13
Komarichev Goergii <71156640+Id3n71@users.noreply.github.com>
2024-03-20 16:37:47 +0300

    Update Task 3.4.sql

--- Task 3.4.sql
+++ Task 3.4.sql
@@ -1,8 +1,7 @@
---Показать список дат выполнения заказа (DueDate), к которым относятся несколько продуктов (ProductID), из таблицы 
+--Показать список продуктов (ProductID), которые имеют несколько дат выполнения заказа (DueDate), из таблицы 
 --Purchasing.PurchaseOrderDetail, используя SELF JOIN.
 
-select pod."DueDate" 
+select distinct pod."ProductID" 
 from "Purchasing"."PurchaseOrderDetail" pod 
-inner join "Purchasing"."PurchaseOrderDetail" pod2 on pod."DueDate" = pod2."DueDate" and pod."ProductID" <> pod2."ProductID"
-group by pod."DueDate"
-having count(distinct  pod."ProductID") > 1;
+inner join "Purchasing"."PurchaseOrderDetail" pod2 on pod."ProductID" = pod2."ProductID"
+where pod."DueDate" <> pod2."DueDate";
